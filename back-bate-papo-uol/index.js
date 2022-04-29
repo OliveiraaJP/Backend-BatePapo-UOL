fed79f09191a41cd30ba3f8c4ab9a3e0f0100054
Joao Paulo <oliveirajpufrj@gmail.com>
2022-04-29 00:40:07 -0300

    feat: register/status message login

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -1,18 +1,68 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import cors from "cors";
 import chalk from "chalk";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
+import Joi from "joi";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
 
-let participants= []
+const mongoClient = new MongoClient(process.env.MONGO_URI); // criando config da conexão
 
-app.get("/participants", (req, res) => {
-    res.send(participants)
-})
+app.post("/participants", async (req, res) => {
+  try {
+    await mongoClient.connect();
+    let database = mongoClient.db("API_UOL");
+
+    const { name } = req.body;
+
+    await database.collection("participants").insertOne({
+      name: name,
+      lastStatus: Date.now(),
+    });
+
+    await database.collection("messages").insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().locale("pt-br").format("HH:mm:ss"),
+    });
+
+    res.sendStatus(201);
+  } catch (e) {
+    console.log(chalk.redBright.bold("error ->"), e);
+    res.sendStatus(500);
+  } finally {
+    mongoClient.close();
+  }
+});
+
+app.get("/participants", async (req, res) => {
+  try {
+    await mongoClient.connect();
+    const database = mongoClient.db("API_UOL");
+
+    const participants = await database
+      .collection("participants")
+      .find({})
+      .toArray();
+
+  } catch (e) {
+    console.log(chalk.redBright.bold("error ->"), e);
+  } finally {
+    mongoClient.close();
+  }
+});
 
 
-app.listen(5000, () =>{
-    console.log(chalk.magentaBright.bold("Server is running on: http://localhost:5000"));
-})
+app.listen(5000, () => {
+  console.log(
+    chalk.magentaBright.bold("Server is running on: http://localhost:5000")
+  );
+});
